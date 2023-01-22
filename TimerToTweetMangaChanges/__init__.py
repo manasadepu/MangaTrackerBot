e552883df66c689efa10ffb207ddaa3ca5291a0c
manasadepu <112828012+manasadepu@users.noreply.github.com>
2023-01-22 11:47:54 -0500

    fixed import issue

--- TimerToTweetMangaChanges/__init__.py
+++ TimerToTweetMangaChanges/__init__.py
@@ -1,14 +1,17 @@
 import datetime
 import logging
 
+from . import mainmagna
 import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
+    print('Inside Main')
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-
+    print('Calling Magna')
+    mainmagna.run()
     if mytimer.past_due:
         logging.info('The timer is past due!')
-
+    print('logging info')
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
