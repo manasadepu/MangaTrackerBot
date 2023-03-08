f306c48689fe6fd431591557aa926325473348bd
manasadepu <112828012+manasadepu@users.noreply.github.com>
2023-03-08 01:03:33 -0500

    fixed parsing issue

--- TimerToTweetMangaChanges/__init__.py
+++ TimerToTweetMangaChanges/__init__.py
@@ -1,16 +1,41 @@
 import datetime
 import logging
-
-from . import mainmagna
+import json
+from . import ranking
+from . import mangatweet as mt
 import azure.functions as func
 
-
-def main(mytimer: func.TimerRequest) -> None:
-    print('Inside Main')
+def main(mytimer: func.TimerRequest, writelist, readlist) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-    print('Calling Magna')
-    mainmagna.run()
+
+    manaslist = ranking.fetch_media_list()
+    
+    manaslist = json.dumps(manaslist)
+    datalist = json.dumps({
+        "ID": 1,
+        "string": manaslist
+    })
+
+    writelist.set(datalist)
+
+    retrieved_list = json.loads(readlist)
+
+    # item = json.loads(pastlist[0]['string'])
+    # print(type(item))
+    # print(item)
+    # print(item['data']['Page']['pageInfo']['total'])
+
+    pastlist = ranking.cleandata_foruse(retrieved_list)
+    new_list = ranking.fetch_media_list()
+    new_list = ranking.cleandata_foruse()
+    media_changes = ranking.get_media_changes_list(pastlist, new_list)
+    tweet_list = ranking.prepare_tweet_text_list(media_changes)
+
+    for tweet in tweet_list:
+        datetimetweet = f"\nThis is generated at {datetime.datetime.now()}\n" + tweet
+        mt.client.create_tweet(text=datetimetweet)
+
     if mytimer.past_due:
         logging.info('The timer is past due!')
     print('logging info')
